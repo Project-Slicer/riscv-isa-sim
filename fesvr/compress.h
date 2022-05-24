4e25e739012577fdc8d8c09bebe083388351adf9
MaxXSoft <x@MaxXSoft.net>
2022-05-24 11:51:41 +0800

    (compress) fixed some details about threshold

--- fesvr/compress.h
+++ fesvr/compress.h
@@ -18,12 +18,10 @@ class compressor_t {
     Error,
   };
 
-  compressor_t(size_t write_buf_size, size_t compress_threshold_num,
-               size_t compress_threshold_den)
+  compressor_t(size_t write_buf_size, float compress_threshold)
       : state_(state_t::Idle),
         write_buf_size_(write_buf_size),
-        compress_threshold_num_(compress_threshold_num),
-        compress_threshold_den_(compress_threshold_den) {}
+        compress_threshold_(compress_threshold) {}
 
   state_t compress(int dirfd, const char *file_name);
   void reset() { state_ = state_t::Idle; }
@@ -33,19 +31,21 @@ class compressor_t {
 
  private:
   bool compress_file(int out_fd, int in_fd);
+  bool rename_file(int olddirfd, const char *oldpath, int newdirfd,
+                   const char *newpath);
 
   static constexpr size_t kFileBufSize = 255, kQueueSize = 255;
   state_t state_;
-  size_t write_buf_size_, compress_threshold_num_, compress_threshold_den_;
+  size_t write_buf_size_;
+  float compress_threshold_;
 };
 
 class compressors_t {
  public:
   compressors_t(size_t num_compressors, size_t write_buf_size,
-                size_t compress_threshold_num, size_t compress_threshold_den)
+                float compress_threshold)
       : compressors_(num_compressors,
-                     compressor_t(write_buf_size, compress_threshold_num,
-                                  compress_threshold_den)) {}
+                     compressor_t(write_buf_size, compress_threshold)) {}
 
   ssize_t compress(int dirfd, const char *file_name);
   compressor_t::state_t take_if_done(size_t i);
